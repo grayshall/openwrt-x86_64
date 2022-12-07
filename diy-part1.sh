c45985ca93ad4088628acf2b4ceaf58a41d37bb3
grayshall <64482280+grayshall@users.noreply.github.com>
2022-12-07 18:48:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,7 +13,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git https://github.com/coolsnowwolf/lede/openwrt-package' feeds.conf.default
 sed -i '$a src-git packages https://github.com/coolsnowwolf/packages' feeds.conf.default
 sed -i '$a src-git luci https://github.com/coolsnowwolf/luci' feeds.conf.default
 sed -i '$a src-git routing https://git.openwrt.org/feed/routing.git' feeds.conf.default
@@ -22,4 +22,4 @@ sed -i '$a src-git freifunk https://github.com/freifunk/openwrt-packages.git' fe
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-sed -i '$a src-git ElonH https://github.com/ElonH/Rclone-OpenWrt' feeds.conf.default
+
