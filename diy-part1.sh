6bf54ed67cf4a1fb656aa61682939f694aea594b
grayshall <64482280+grayshall@users.noreply.github.com>
2022-12-07 19:34:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,12 +13,12 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #
 # Add a feed source
-sed -i 'src-git packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
-sed -i 'src-git luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
-sed -i 'src-git routing https://git.openwrt.org/feed/routing.git' >>feeds.conf.default
-sed -i 'src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
-sed -i 'src-git freifunk https://github.com/freifunk/openwrt-packages.git' >>feeds.conf.default
-sed -i 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-sed -i 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-sed -i 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+sed -i 's src-git packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
+sed -i 's src-git luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
+sed -i 's src-git routing https://git.openwrt.org/feed/routing.git' >>feeds.conf.default
+sed -i 's src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
+sed -i 's src-git freifunk https://github.com/freifunk/openwrt-packages.git' >>feeds.conf.default
+sed -i 's src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+sed -i 's src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+sed -i 's src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
